f138695833559d4b10dcca1cb90e557faa054a7a
jhackmeyer <jhackmeyer@ucla.edu>
2021-10-03 22:08:56 -0700

    Adding table of p-values for all t-tests

--- MechaCarChallenge.R
+++ MechaCarChallenge.R
@@ -6,3 +6,11 @@ summary(lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_cleara
 suspension_coil_df <- read.csv(file='resources/Suspension_Coil.csv', check.names=F, stringsAsFactors = F)
 suspension_summary <- suspension_coil_df %>% summarize(Mean=mean(PSI), Median=median(PSI), Variance=var(PSI),SD=sd(PSI), .groups='keep')
 lot_summary <- suspension_coil_df %>% group_by(Manufacturing_Lot) %>% summarize(Mean=mean(PSI), Median=median(PSI), Variance=var(PSI),SD=sd(PSI), .groups='keep')
+
+t_all <- t.test(suspension_coil_df$PSI, mu=1500)
+t_1 <- t.test(subset(suspension_coil_df, Manufacturing_Lot=="Lot1")$PSI, mu=1500)
+t_2 <- t.test(subset(suspension_coil_df, Manufacturing_Lot=="Lot2")$PSI, mu=1500)
+t_3 <- t.test(subset(suspension_coil_df, Manufacturing_Lot=="Lot3")$PSI, mu=1500)
+
+p_table <- data.frame("All"=t_all$p.value, "Lot 1"=t_1$p.value, "Lot 2"=t_2$p.value, "Lot 3"=t_3$p.value)
+rownames(p_table) <- c("p-Values")

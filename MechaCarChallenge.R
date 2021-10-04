bd56cf390c5ebb6510105d4c9560e112c3a3dac4
jhackmeyer <jhackmeyer@ucla.edu>
2021-10-03 22:38:00 -0700

    small fix

--- MechaCarChallenge.R
+++ MechaCarChallenge.R
@@ -1,7 +1,7 @@
 library(dplyr)
 mechaCar_data_df <- read.csv(file='resources/MechaCar_mpg.csv', check.names=F, stringsAsFactors = F)
 lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD,data=mechaCar_data_df)
-summary(lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD,data=MechaCar_data_df))
+summary(lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD,data=mechaCar_data_df))
 
 suspension_coil_df <- read.csv(file='resources/Suspension_Coil.csv', check.names=F, stringsAsFactors = F)
 suspension_summary <- suspension_coil_df %>% summarize(Mean=mean(PSI), Median=median(PSI), Variance=var(PSI),SD=sd(PSI), .groups='keep')

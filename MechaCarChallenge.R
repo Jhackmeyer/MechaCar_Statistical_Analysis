25034c9b0679c051486f3852b4de092151899660
jhackmeyer <jhackmeyer@ucla.edu>
2021-10-03 20:31:31 -0700

    Adding summary statistics tables

--- MechaCarChallenge.R
+++ MechaCarChallenge.R
@@ -1,4 +1,8 @@
 library(dplyr)
-MechaCar_data_df <- read.csv(file='resources/MechaCar_mpg.csv', check.names=F, stringsAsFactors = F)
-lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD,data=MechaCar_data_df)
+mechaCar_data_df <- read.csv(file='resources/MechaCar_mpg.csv', check.names=F, stringsAsFactors = F)
+lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD,data=mechaCar_data_df)
 summary(lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD,data=MechaCar_data_df))
+
+suspension_coil_df <- read.csv(file='resources/Suspension_Coil.csv', check.names=F, stringsAsFactors = F)
+suspension_summary <- suspension_coil_df %>% summarize(Mean=mean(PSI), Median=median(PSI), Variance=var(PSI),SD=sd(PSI), .groups='keep')
+lot_summary <- suspension_coil_df %>% group_by(Manufacturing_Lot) %>% summarize(Mean=mean(PSI), Median=median(PSI), Variance=var(PSI),SD=sd(PSI), .groups='keep')
